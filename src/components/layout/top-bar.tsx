6eb222281609dc7ed6219f7bc21f8528c89d7403
Victoria Rose <240325296+victoria-rose@users.noreply.github.com>
2026-08-05 13:03:38 +0530

    feat: redirect update endpoints and links to fork repo

--- src/components/layout/top-bar.tsx
+++ src/components/layout/top-bar.tsx
@@ -306,7 +306,7 @@ function ThemeSubMenu() {
   );
 }
 
-const REPO_ISSUES_URL = "https://github.com/NUber-dev/YTubic/issues/new";
+const REPO_ISSUES_URL = "https://github.com/victoria-rose/YTubic/issues/new";
 
 /**
  * Feedback form that hands off to GitHub: Submit opens a prefilled
